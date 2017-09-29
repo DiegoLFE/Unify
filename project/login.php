93e27e83fbac04e6a817336940ac05bebd714982
DiegoLFE <diegolf1000@gmail.com>
2017-09-29 18:55:14 -0300

    ejemplo super claro en la carpeta mysqli

--- project/login.php
+++ project/login.php
@@ -7,7 +7,8 @@
     }
     return false;
   }
-
+    decode("usuario");
+    
 
   $_SESSION["usuario"]
 
@@ -44,6 +45,58 @@ NO SE OLVIDEN DE PONER SESSION_START() EN CADA PÁGINA
 7 - Botón de deslogueo
   session_destroy()
   setcookie("nombredecookie", "", time()-1);
+*/
+
+
+
+/*DATOS QUE PUEDEN SERVIR
+
+<?php
+session_start();
+?>
+
+<?php
+
+$host_db = "localhost";
+$user_db = "root";
+$pass_db = "";
+$db_name = "basedatosmaster";
+$tbl_name = "Usuarios";
+
+$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM $tbl_name WHERE nombre_usuario = '$username'";
+
+$result = $conexion->query($sql);
+
+
+if ($result->num_rows > 0) {
+ }
+ $row = $result->fetch_array(MYSQLI_ASSOC);
+ if (password_verify($password, $row['password'])) {
+
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    $_SESSION['start'] = time();
+    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+
+    echo "Bienvenido! " . $_SESSION['username'];
+    echo "<br><br><a href=panel-control.php>Panel de Control</a>";
+
+ } else {
+   echo "Username o Password estan incorrectos.";
+
+   echo "<br><a href='login.html'>Volver a Intentarlo</a>";
+ }
+ mysqli_close($conexion);
+
 */
  ?>
 
