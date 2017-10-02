b6cb5c00d3231490b54dfda21c3dad733e794e14
Diego <diegolf1000@gmail.com>
2017-10-01 22:04:04 -0300

    cambios minimos

--- project/login.php
+++ project/login.php
@@ -1,18 +1,31 @@
 <?php
   var_dump ($_SESSION['logged']);
 
+  $user_cookie =
+
   function estaLogueado() {
     if (isset($_SESSION["usuario"])) {
       return true;
     }
     return false;
   }
-    decode("usuario");
-    
+
+  $json = decode("/db/usuarios.json");
+
+  file_get_contents($json);
+
+  foreach ($variable as $key => $value) {
+        if($value["email"] == $_POST['email']) {
+          echo "existe";
+          password_verify()
+  }
+  }
+
+
 
   $_SESSION["usuario"]
 
-$_SESSION["usuario"] = $_COOKIE["usuario"]
+  $_SESSION["usuario"] = $_COOKIE["usuario"]
 
 
   $_COOKIE["usuario"]
@@ -47,57 +60,6 @@ NO SE OLVIDEN DE PONER SESSION_START() EN CADA PÁGINA
   setcookie("nombredecookie", "", time()-1);
 */
 
-
-
-/*DATOS QUE PUEDEN SERVIR
-
-<?php
-session_start();
-?>
-
-<?php
-
-$host_db = "localhost";
-$user_db = "root";
-$pass_db = "";
-$db_name = "basedatosmaster";
-$tbl_name = "Usuarios";
-
-$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
-
-if ($conexion->connect_error) {
- die("La conexion falló: " . $conexion->connect_error);
-}
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$sql = "SELECT * FROM $tbl_name WHERE nombre_usuario = '$username'";
-
-$result = $conexion->query($sql);
-
-
-if ($result->num_rows > 0) {
- }
- $row = $result->fetch_array(MYSQLI_ASSOC);
- if (password_verify($password, $row['password'])) {
-
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username;
-    $_SESSION['start'] = time();
-    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
-
-    echo "Bienvenido! " . $_SESSION['username'];
-    echo "<br><br><a href=panel-control.php>Panel de Control</a>";
-
- } else {
-   echo "Username o Password estan incorrectos.";
-
-   echo "<br><a href='login.html'>Volver a Intentarlo</a>";
- }
- mysqli_close($conexion);
-
-*/
  ?>
 
 <!DOCTYPE html>
